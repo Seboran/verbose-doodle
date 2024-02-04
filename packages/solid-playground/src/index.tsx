33251499d77a7850456ddb9f0b3d0444272bb4e1
Nirina Rabeson <nirinarabeson@me.com>
2024-02-04 17:12:51 +0100

    prettier et cleanup

--- packages/solid-playground/src/index.tsx
+++ packages/solid-playground/src/index.tsx
@@ -1,13 +1,13 @@
-import { render } from 'solid-js/web';
+import { render } from 'solid-js/web'
 
-import { TodoList } from './todo-list';
+import { TodoList } from './todo-list'
 
-const root = document.getElementById('root');
+const root = document.getElementById('root')
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   throw new Error(
     'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
-  );
+  )
 }
 
-render(() => <TodoList />, root!);
+render(() => <TodoList />, root!)
